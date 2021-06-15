95b4d64419982101a2e32f71d150b24ebf16ccfa
Kazuno0101 <nunoalwi@gmail.com>
2021-06-15 10:41:53 +0700

    add manga
    (+) berserk done
    (+) full metal done

---
+++ img/berserk/cp 1/scraping2.py
@@ -0,0 +1,38 @@
+from selenium.common.exceptions import NoSuchElementException
+from selenium import webdriver
+from datetime import datetime
+import urllib.request
+import json
+
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+driver.get("https://komikindo.co/berserk-chapter-1/")
+
+manga = []
+i = 1
+
+now = datetime.now()
+waktu = now.strftime("%d %B %Y %H:%M:%S")
+
+while i <= 150:   
+    for parrent in driver.find_elements_by_tag_name("p"):
+        print(parrent.text.split("\n"))
+        for img in parrent.find_elements_by_tag_name("img"):
+            print(img.get_attribute("src")) 
+            urllib.request.urlretrieve(img.get_attribute("src"), str(i)+".png")
+            i = i+1
+            manga.append({
+                "Image": img.get_attribute("src"),
+                "Waktu_scrapping":waktu
+            })
+
+    try:
+        driver.find_element_by_class_name("desc").find_element_by_partial_link_text("Next").click()
+    except NoSuchElementException as e:
+        break
+
+hasil_scraping = open("hasilscraping.json", "w")
+json.dump(manga, hasil_scraping, indent = 6)
+hasil_scraping.close()
+driver.quit()
